39b191f80a20b879df761f81dcb5b0b65565155d
abdelhakim <siroukane.abdelhakim@gmail.com>
2024-07-22 13:24:30 +0200

    Finish the Home page. Set the single page and its components Add, CostumizeProducts, Filter and ProductImage

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from "next/image";
+import Filter from "../components/Filter";
+import ProductList from "../components/ProductList";
 
 const ListPage = () => {
   return (
-    <div>ListPage</div>
-  )
-}
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* CAMPAIGN */}
+      <div className="bg-pink-50 px-4 justify-between h-64 hidden sm:flex">
+        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-semibold leading-[48px] to-gray-700">
+            Grab up to 50% off on <br /> Selected Products
+          </h1>
+          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
+            Buy Now
+          </button>
+        </div>
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt="" fill className="object-contain" />
+        </div>
+      </div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
+      <ProductList />
+    </div>
+  );
+};
 
-export default ListPage
+export default ListPage;
